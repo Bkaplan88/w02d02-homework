aa4248d8e46eb82990cb945498437951dd386afe
Benjamin Kaplan <benjaminkaplanart@gmail.com>
2018-06-20 20:43:30 -0600

    created game logic

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const player = {
 
 }
 const game = {
-	availableCards: [
+	cards: [
 	 {name: "Bulbasaur", damage: 60},
 	 {name: "Caterpie", damage: 40},
 	 {name: "Charmander", damage: 60},
@@ -31,59 +31,228 @@ const game = {
 	 {name: "Vulpix", damage: 50}, 
 	 {name: "Weedle", damage: 40}
 	 ],
-	playerHand: [],
+	round: 1,
 	computerHand: [],
-	playerRoundsWon: 0,
 	computerRoundsWon: 0,
-
+	computerPoints: 0,
+	playedCards: [],
 	deal() {
-		let randomIndex = Math.floor(Math.random() * this.availableCards.length);
-		let dealCard = game.availableCards.splice(randomIndex, 1);
+		let randomIndex = Math.floor(Math.random() * this.cards.length);
+		let dealCard = this.cards.splice(randomIndex, 1)[0];
 		return dealCard;
+
 	},
-	createComputerHand() {
+	dealCards() {
 		for (let i = 0; i < 3; i++) {
+			player.currentHand.push(this.deal());
 			this.computerHand.push(this.deal());
-			
 		}
-		return this.computerHand;
 	},
-	createPlayerHand() {
-		for (let i = 0; i < 3; i++) {
-			this.playerHand.push(this.deal());
-			
-		}
-		return this.playerHand;
-	},
-	roundWin() {
-		let playerPoints = 0;
-		let computerPoints = 0;
-		for (let i = 0; i < this.playerHand.length; i++) {
-			console.log(`player played ${this.playerHand[i].name} card`);
-			console.log(`computer played ${this.computerHand[i].name} card`);
-			if (this.playerHand[i].damage > this.computerHand[i].damage) {
-				playerPoints++;
-			} else if (this.computerHand[i].damage > this.playerHand[i].damage) {
-				computerPoints++;
+	playerChoice() {
+		let playerChoice = prompt(`Your hand is ${JSON.stringify(player.currentHand)}. Pick a pokemon to play.`);
+		for (let i = 0; i < player.currentHand.length; i++) {
+			if (playerChoice === player.currentHand[i].name) {
+				let playerCard = playerChoice;
 			}
-  		}
-  		if (playerPoints > computerPoints) {
-  			this.playerRoundsWon++;
-  			console.log("Player won round");
+		}
+	},
+	battle() {
+		this.playerChoice();
+		let playerCard = player.currentHand.splice(0, 1)[0];
+		let computerCard = this.computerHand.splice(0, 1)[0];
+		alert(`You played ${JSON.stringify(playerCard)}`);
+		alert(`computer played ${JSON.stringify(computerCard)}`);
+		if (playerCard.damage > computerCard.damage) {
+			alert("You win");
+			player.points++;
+			console.log(`Player has ${player.points} points.`)
+		} else if (computerCard.damage > playerCard.damage) {
+			alert("computer wins");
+			this.computerPoints++;
+			console.log(`The computer has ${this.computerPoints} points.`);
+		}
+		this.playedCards.push(playerCard, computerCard);
+	},
+	playRound() {
+		this.dealCards();
+		for (let i = 0; i < 3; i++) {
+			this.battle();
+		if (player.points > this.computerPoints) {
+			player.roundsWon++;
+			console.log(`The player won this round. ${player.roundsWon} rounds won.`);
+		} else if (this.computerPoints > player.points) {
+			this.computerRoundsWon++;
+			}
+		}
+		player.points = 0;
+		this.computerPoints = 0;
+		this.round++;
 
-  		} else if (computerPoints > playerPoints) {
-  			this.computerRoundsWon++;
-  			console.log("Computer won round");
-
-  		}
- }
+	},
+	playGame() {
+		while (this.cards.length > 5) {
+		this.playRound();
+		}
+		if(player.roundsWon > this.computerRoundsWon){
+			alert("Game Over You Won!");
+		} else if (this.computerRoundsWon > player.roundsWon) {
+			alert("Game Over Computer Wins.");
+		}
+		
+	},
 };
 
+game.playGame();
 
 
 
-console.log(game.deal());
-console.log(game.createComputerHand());
-console.log(game.createPlayerHand());
-game.roundWin();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+// 	},
+// 	createComputerHand() {
+// 		for (let i = 0; i < 3; i++) {
+// 			this.computerHand.push(this.deal());
+// 			console.log(`Computer has been dealt ${this.deal()[i]}`);
+			
+// 		}
+// 		return this.computerHand;
+// 	},
+// 	createPlayerHand() {
+// 		for (let i = 0; i < 3; i++) {
+// 			this.playerHand.push(this.deal());
+// 			console.log(`Player had been dealt ${this.deal()}`);
+			
+// 		}
+// 		return this.playerHand;
+// 	},
+
+// 	round() {
+// 		console.log(`Player played ${this.playerHand[i].name} card`);
+// 		console.log(`Computer played ${this.computerHand[i].name} card`);
+// 		for (let i = 0; i < 3; i++) {
+// 			if (this.computerHand[i].damage > this.playerHand[i].damage); {
+// 				console.log()
+// 			}
+
+
+// 		}
+// 	}
+
+// 	// roundWin() {
+// 	// 	let playerPoints = 0;
+// 	// 	let computerPoints = 0;
+// 	// 	for (let i = 0; i < this.playerHand.length; i++) {
+// 	// 		console.log(`player played ${(this.playerHand[i].name)} card`);
+// 	// 		console.log(`computer played ${this.computerHand[i].name} card`);
+// 	// 		if (this.playerHand[i].damage > this.computerHand[i].damage) {
+// 	// 			playerPoints++;
+// 	// 		} else if (this.computerHand[i].damage > this.playerHand[i].damage) {
+// 	// 			computerPoints++;
+// 	// 		}
+//  //  		}
+//  //  		if (playerPoints > computerPoints) {
+//  //  			this.playerRoundsWon++;
+//  //  			console.log("Player won round");
+
+//  //  		} else if (computerPoints > playerPoints) {
+//  //  			this.computerRoundsWon++;
+//  //  			console.log("Computer won round");
+
+//  //  		}
+//  // 	},
+//  // 	startGame() {
+//  // 		for (let i = 0; i < this.availableCards.lengths; i++) {
+//  // 		this.deal();
+//  // 		this.createPlayerHand();
+//  // 		this.createComputerHand();
+//  // 		this.roundWin();
+//  // 			if (this.availableCards.length < 6) {
+//  // 				console.log("game over");
+
+//  // 			}
+
+//  // 		}
+//  // 	},
+// };
+
+
+// console.log(game.createComputerHand());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
